39af6ec94f28a5372a69089757e6d57547f02b83
Yorkd1 <davidgyork@gmail.com>
2024-02-07 17:11:14 -0700

    Added content to Login.js

---
+++ Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+export default function Login(props){
+    return(
+        <View style={styles.login}>
+            <Text>This is the Login Screen</Text>
+            <Button title="Log In David" onPress={()=>{
+                console.log('Hello David!')
+            }}></Button>
+            <Button title="Log In" onPress={()=>{
+                console.log('')
+            }}></Button>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    login: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        backgroundColor: 'green',
+        height: '12%',
+        alignItems: 'flex-end',
+        paddingBottom: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        
+      },
+})
